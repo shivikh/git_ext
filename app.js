6beb95173fd9b8b2176f185e46d646dc584e3c5e
shivani <skhandelwal58821@imad-feb-18-ssh.c.hasura-imad.internal>
2018-04-22 11:04:38 +0000

    New feature

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+function bigCode()
+{
+ console.log('writing lot of code');
+}
 function something()
 {
  console.log('I am writing something');
 }
 console.log('Hello World');
 function something();
+function bigCode();
